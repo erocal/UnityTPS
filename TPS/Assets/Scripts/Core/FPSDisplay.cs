b4ab52f7e391db55e95f7f15087e4ff93591fb0d
erocal <s159752zxcvbn1@gmail.com>
2024-04-06 17:12:56 +0800

    [!][+]解決卡頓，印出FPS

---
+++ TPS/Assets/Scripts/Core/FPSDisplay.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSDisplay : MonoBehaviour
+{
+
+    #region -- 資源參考區 --
+
+    [Header("Text")]
+    [SerializeField, Tooltip("FPS")] private Text Text_FPS;
+    [SerializeField, Tooltip("ms")] private Text Text_ms;
+
+    #endregion
+
+    #region -- 變數參考區 --
+
+    float deltaTime = 0.0f;
+    float fps = 0.0f;
+    float msec = 0.0f;
+
+    #endregion
+
+    #region -- 初始化/運作 --
+
+    void Awake()
+    {
+        // 標記此物件不會在場景切換時被卸載
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        Text_FPS.text = $"{fps:0} fps";
+        Text_ms.text = $"{msec:0.0} ms";
+    }
+
+    #endregion
+}
