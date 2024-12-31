be55a5b71c618fb27efcba01bec23814e9d25e6e
Sean <s159752zxcvbn1@gmail.com>
2024-12-31 17:02:56 +0800

    [!][+]RadialBlur

---
+++ Assets/Scripts/RadiusBlur/RadiusBlur.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class RadiusBlur : MonoBehaviour
+{
+
+    #region -- 變數參考區 --
+
+    static int radius_blur_dataId = Shader.PropertyToID("_RadiusData"),
+        radius_blur_iterationId = Shader.PropertyToID("_RadiusIterationData"),
+        radius_blur_centerRange = Shader.PropertyToID("_RadiusCenterRange");
+
+    public Material radius_material;
+
+    public Vector3 radius_data
+    {
+        get{
+            if(radius_material != null)
+            {
+                return radius_material.GetVector(radius_blur_dataId);
+            }
+            return new Vector3(0.5f, 0.5f, 0.01f);
+        }
+        set{
+            if(radius_material != null)
+            {
+                radius_material.SetVector(radius_blur_dataId, value);
+            }
+        }
+    }
+
+    public int iteration{
+        get{
+            if(radius_material != null)
+            {
+                return (int)radius_material.GetVector(radius_blur_iterationId).x;
+            }
+            return 1;
+        }
+        set{
+            if(radius_material != null)
+            {
+                float invIteration = 1.0f / value;
+                radius_material.SetVector(radius_blur_iterationId, new Vector4(value, invIteration, 0f, 0f));
+            }
+        }
+    }
+    public float radius_center_range{
+        get{
+            if(radius_material != null)
+            {
+                return radius_material.GetFloat(radius_blur_centerRange);
+            }
+            return 0f;
+        }
+        set{
+            if(radius_material != null)
+            {
+                if(value <= 0f)
+                {
+                    radius_material.DisableKeyword("_USE_CIRCLE_CENTER");
+                }
+                else
+                {
+                    radius_material.EnableKeyword("_USE_CIRCLE_CENTER");
+                }
+                radius_material.SetFloat(radius_blur_centerRange, value);
+            }
+        }
+    }
+
+    private const int RADIUS_BLUR_PASS = 0;
+
+    #endregion
+
+    #region -- 初始化/運作 --
+
+    private void Awake() {
+        if(radius_material != null)
+        {
+            radius_data = radius_material.GetVector(radius_blur_dataId);
+            iteration = (int)radius_material.GetVector(radius_blur_iterationId).x;
+            radius_center_range = radius_material.GetFloat(radius_blur_centerRange);
+        }
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        if(radius_material)
+        {
+            Graphics.Blit(src, dest, radius_material, RADIUS_BLUR_PASS);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+
+    #endregion
+
+}
---
+++ Assets/Shaders/RadiusBlur/RadiusBlur.shader
@@ -0,0 +1,86 @@
+﻿Shader "SaberShad/RadiusBlur"
+{
+    Properties
+    {
+        [HideInInspector]_MainTex ("Texture", 2D) = "white" {}
+        // 徑向模糊數據 xy分量代表徑向中心點  z分量代表偏移 
+        _RadiusData("Radius Data", Vector) = (0.5, 0.5, 0.0, 1.0)
+        // y分量代表迭代次數的倒數 例如迭代30次就是1/30
+        _RadiusIterationData("Radius Iteration", Vector) = (1.0, 1.0, 0.0, 0.0)
+        _RadiusCenterRange("Radius Center Range", Range(0, 1.5)) = 0
+    }
+    SubShader
+    {
+        CGINCLUDE
+            #include "UnityCG.cginc"
+            #pragma shader_feature _ _USE_CIRCLE_CENTER
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float4 _MainTex_TexelSize;
+            half3 _RadiusData;
+            half2 _RadiusIterationData;
+            half _RadiusCenterRange;
+
+            struct a2f_rb
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f_rb
+            {
+                float4 vertex : SV_POSITION;
+                float2 texcoord: TEXCOORD0;
+            };
+
+            v2f_rb RadiusBlurVertex(a2f_rb v)
+            {
+                v2f_rb o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.texcoord = TRANSFORM_TEX(v.uv, _MainTex);
+
+                return o;
+            }
+
+            half4 RadiusBlurFragment(v2f_rb i): SV_Target
+            {
+
+                half2 newTexcoord = i.texcoord.xy;
+
+                half aspectRatio = _ScreenParams.x / _ScreenParams.y; // 計算屏幕寬高比
+
+                half2 radiusRange = ( _RadiusData.xy - newTexcoord.xy );
+
+                radiusRange.x *= aspectRatio;
+
+                half4 color = 0.0;
+                half range_amount = 1;
+                #if defined(_USE_CIRCLE_CENTER)
+                    // 點乘獲得偏移的平方
+                    // 不使用开方获得真实的距离，直接用平方来比较
+                    half square_dis = dot(radiusRange, radiusRange);
+                    half square_range = _RadiusCenterRange * _RadiusCenterRange;
+
+                    range_amount = saturate((square_dis - square_range) / square_range);
+                #endif
+                [unroll(30)]
+                for(int j = 0; j < _RadiusIterationData.x; j++)
+                {
+                    color += tex2D(_MainTex, newTexcoord);
+                    newTexcoord += radiusRange * _RadiusData.z * range_amount;
+                }
+
+                return color * _RadiusIterationData.y;
+            }
+        ENDCG
+
+        Cull Off ZWrite Off ZTest Always
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex RadiusBlurVertex
+			#pragma fragment RadiusBlurFragment
+			ENDCG
+		}
+    }
+}
