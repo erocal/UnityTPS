89f4f6ef17ce4626f21649176bd3feae81e2e3db
Sean <s159752zxcvbn1@gmail.com>
2025-01-08 17:19:21 +0800

    [!][-]250108

--- Assets/Scripts/Core/Log.cs
+++ Assets/Scripts/Core/Log.cs
@@ -1,20 +1,25 @@
-using System;
 using UnityEngine;
 
 public static class Log
 {
     public static void Warning(string msg)
     {
+#if UNITY_EDITOR
         Debug.LogWarning(msg);
+#endif
     }
 
     public static void Info(string msg)
     {
+#if UNITY_EDITOR
         Debug.Log(msg);
+#endif
     }
 
     public static void Error(string msg)
     {
+#if UNITY_EDITOR
         Debug.LogError(msg);
+#endif
     }
 }
